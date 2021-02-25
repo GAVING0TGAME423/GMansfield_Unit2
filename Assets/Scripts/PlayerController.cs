b45d16523d3e3ad53e7673c384917baf99d6ab6e
GAVING0TGAME423 <78512752+GAVING0TGAME423@users.noreply.github.com>
2021-02-24 22:16:54 -0800

    Lesson A
    Movement, Clipped Bounds.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float HorizontalInput;
+    public float speed = 10;
+    private float xbound = 20;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+      HorizontalInput =  Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * HorizontalInput * Time.deltaTime * speed );
+
+        if(transform.position.x > xbound)
+        {
+            transform.position = new Vector3(xbound, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -xbound)
+        {
+            transform.position = new Vector3(-xbound, transform.position.y, transform.position.z);
+        }
+    }
+}
