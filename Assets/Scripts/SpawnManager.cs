53cbac1041fb404a200cb22356d9dc95a42a97b3
GAVING0TGAME423 <78512752+GAVING0TGAME423@users.noreply.github.com>
2021-03-03 15:27:31 -0800

    Lesson C
    animals spawn when A is pressed, orthographic game camera, radnom animal and location spawn

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalprefabs;
+    private float XPositionRange = 19;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            float randomXposition = Random.Range(-XPositionRange, XPositionRange);
+            int animalprefabIndex = Random.Range(0, animalprefabs.Length);
+            Vector3 random = new Vector3(randomXposition, 0, 21);
+            Instantiate(animalprefabs[animalprefabIndex], random,
+                animalprefabs[animalprefabIndex].transform.rotation);
+        }
+    }
+}
